0c867c152091d6b29b8a5fe1c4f9c0b70413a596
medali.bettaieb <medali.bettaieb@esprit.tn>
2015-04-27 15:17:59 +0100

    login
    Signed-off-by: medali.bettaieb <medali.bettaieb@esprit.tn>

--- e-safra-web/src/main/java/mBeans/LoginBean.java
+++ e-safra-web/src/main/java/mBeans/LoginBean.java
@@ -5,6 +5,8 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import services.interfaces.IdentificationServiceLocal;
+import domain.BusMan;
+import domain.Driver;
 import domain.User;
 
 @ManagedBean
@@ -16,13 +18,19 @@ public class LoginBean {
 	private IdentificationServiceLocal identificationServiceLocal;
 
 	public String doLogin() {
-		String navigateTo = "";
+		String navigateTo = "/error";
 		User userFound = identificationServiceLocal.login(user.getLogin(),
 				user.getPassword());
 		if (userFound != null) {
-			navigateTo = "/pages/passengerHome";
-		} else {
-			navigateTo = "/error";
+			user=userFound;
+			if (userFound instanceof BusMan) {
+				navigateTo = "/pages/busMan/home";
+			}
+			if (userFound instanceof Driver) {
+				navigateTo = "/pages/driver/home";
+			} else {
+				navigateTo = "/pages/passenger/home";
+			}
 		}
 		return navigateTo;
 	}
